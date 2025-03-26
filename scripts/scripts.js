aef08ab55216c8a285ddf85b46fbb9b1f4024f3a
EkaterinaGorohova <ekaterinagorohova55@gmail.com>
2025-03-26 17:01:51 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -305,5 +305,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.error('Ошибка при загрузке данных:', error);
             });
     }
-});
- 
+        // Preloader страницы
+        const preloader = document.querySelector('.preloader');
+        const content = document.querySelector('.content');
+        if (preloader && content) {
+            setTimeout(() => {
+                // Скрываем прелоадер
+                preloader.style.opacity = '0';
+                preloader.style.visibility = 'hidden';
+    
+                // Показываем контент
+                content.style.display = 'block';
+    
+                // Удаляем элемент из DOM
+                preloader.remove();
+            }, 3000); // Задержка 3 секунды
+        }
+    });
+
+
+
